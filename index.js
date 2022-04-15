689d992f5976b6ee49f53639136ef2797587fd44
Henrique <74209949+henriquemachad0@users.noreply.github.com>
2022-04-15 18:57:27 -0300

    commit register e logout

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.use('/', authRoutes)
 app.get('/', ToughtController.showToughts)
 
 conn
-    //.sync({ force: true })
+    // .sync({ force: true })
     .sync()
     .then(() => {
         app.listen(3000)
