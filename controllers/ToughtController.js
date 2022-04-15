a807b1596e3b6902e1e523e7e48a5136f27e01e3
Henrique <74209949+henriquemachad0@users.noreply.github.com>
2022-04-15 16:49:58 -0300

    commit primeira rota

---
+++ controllers/ToughtController.js
@@ -0,0 +1,10 @@
+const res = require('express/lib/response')
+const Tought = require('../models/Tought')
+const User = require('../models/User')
+
+
+module.exports = class ToughtController{
+    static async showToughts(req, res){
+        res.render('toughts/home')
+    }
+}
