a8238ca715f17456ed17df012bb1fae8eaaea96e
Henrique <74209949+henriquemachad0@users.noreply.github.com>
2022-04-15 18:16:34 -0300

    commit página de registro e flash messages

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,9 +1,27 @@
-module.exports = class AuthController {
-     static login(req, res) {
-        res.render('auth/login')
-     }
+const User = require('../models/User')
 
-     static register(req, res) {
+const bcrypt = require('bcryptjs')
+
+module.exports = class AuthController {
+    static login(req, res) {
+        res.render('auth/login')
+    }
+
+    static register(req, res) {
         res.render('auth/register')
-     }
+    }
+
+    static async registerPost(req, res) {
+
+        const { name, email, password, confirmpassword } = req.body
+
+        // password match validation
+        if (password != confirmpassword) {
+            req.flash('message', 'As senhas não conferem, tente novamente!')
+            res.render('auth/register')
+
+            return
+        }
+
+    }
 }
