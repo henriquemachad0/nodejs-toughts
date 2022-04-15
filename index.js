b9bd08af623eefb515f3064d5f389336288c1e94
Henrique <74209949+henriquemachad0@users.noreply.github.com>
2022-04-15 17:17:01 -0300

    commit início autenticação

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const User = require('./models/User')
 
 // Import Routes
 const toughtsRoutes = require('./routes/toughtsRoutes')
+const authRoutes = require('./routes/authRoutes')
 
 // Import Controller
 const ToughtController = require('./controllers/ToughtController')
@@ -66,6 +67,7 @@ app.use((req, res, next) => {
 
 // Routes
 app.use('/toughts', toughtsRoutes)
+app.use('/', authRoutes)
 
 app.get('/', ToughtController.showToughts)
 
