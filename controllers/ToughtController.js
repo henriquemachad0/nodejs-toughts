6dddb83965fbfdffb2893c70ec482234b9d43346
Henrique <74209949+henriquemachad0@users.noreply.github.com>
2022-04-18 11:52:02 -0300

    commit final

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -2,10 +2,42 @@ const res = require('express/lib/response')
 const Tought = require('../models/Tought')
 const User = require('../models/User')
 
+const { Op } = require('sequelize')
 
 module.exports = class ToughtController {
     static async showToughts(req, res) {
-        res.render('toughts/home')
+
+        let search = ''
+
+        if (req.query.search) {
+            search = req.query.search
+        }
+
+        let order = 'DESC'
+
+        if(req.query.order === 'old'){
+            order = 'ASC'
+        } else{
+            order = 'DESC'
+        }
+
+        const toughtsData = await Tought.findAll({
+            include: User,
+            where: {
+                title: { [Op.like]: `%${search}%` }
+            },
+            order: [['createdAt', order]],
+        })
+
+        const toughts = toughtsData.map((result) => result.get({ plain: true }))
+        
+        let toughtsQty = toughts.length
+
+        if(toughtsQty === 0){
+            toughtsQty = false
+        }
+
+        res.render('toughts/home', { toughts, search, toughtsQty })
     }
 
     static async dashboard(req, res) {
@@ -28,7 +60,7 @@ module.exports = class ToughtController {
 
         let emptyToughts = false
 
-        if(toughts.length === 0){
+        if (toughts.length === 0) {
             emptyToughts = true
         }
 
@@ -76,6 +108,37 @@ module.exports = class ToughtController {
         } catch (error) {
             console.log('Aconteceu um erro:' + error)
         }
+
+    }
+
+    static async updateTought(req, res) {
+        const id = req.params.id
+
+        const tought = await Tought.findOne({ where: { id: id }, raw: true })
+
+        res.render('toughts/edit', { tought })
+    }
+
+    static async updateToughtSave(req, res) {
+
+        const id = req.body.id
+
+        const tought = {
+            title: req.body.title,
+        }
+
+        try {
+            await Tought.update(tought, { where: { id: id } })
+
+            req.flash('message', 'Pensamento atualizado com sucesso!')
+
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+        } catch (error) {
+            console.log('Aconteceu um erro:' + error)
+        }
+
 
     }
 }
